737511a3b23c8f7d2ba3deea477d9231c4554013
Bruno Seraguza <shagrathkampf@gmail.com>
2023-01-18 16:03:00 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿using DesafioPOO.Models;
+Nokia nk = new Nokia("996589875", "Startack", "00654", 32);
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+nk.InstalarAplicativo("Telegram");
+nk.Ligar();
+nk.ReceberLigacao();
+System.Console.WriteLine($"O cel é: {nk.Numero}, do modelo: {nk.Modelo}," +
+ $" com o Imei: {nk.IMEI}, e com: {nk.Memoria} de memoria ");
+
+
+
+Iphone iph = new Iphone("81174589", "Iphone 11", "00123", 64);
+iph.InstalarAplicativo("Binance");
+iph.Ligar();
+iph.ReceberLigacao();
+System.Console.WriteLine($"O cel é: {iph.Numero}, do modelo: {iph.Modelo}," +
+ $" com o imei: {iph.IMEI}, e com: {iph.Memoria} de memoria ");
+
+
+
+
+
+
+
